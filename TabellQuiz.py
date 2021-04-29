27ba00ef74fb93af4b5f7c0734e5c51a1ca6fe0a
Stark-Alex88 <83231434+Stark-Alex88@users.noreply.github.com>
2021-04-29 22:47:58 +0200

    Update TabellQuiz.py

--- TabellQuiz.py
+++ TabellQuiz.py
@@ -1,26 +1,32 @@
 import random
+
 print(" Ciao Matematici!!! \n Per ogni risposta esatta riceverai 1 Punto \n")
-file= open("tabelline.txt", "r")
+
+file= open("tabelline.txt", "r")   # leggo il contenuto del file
 righe= file.readlines()
 file.close()
-partite= 0
-n= []
-punteggio= 0
-fatte= []
-valido= True
+# creo variabili
+partite= 0   #una come contatore partite
+n= []   #una lista che mi mostra tutte le possibili partite
+punteggio= 0   #una che conteggia i punti
+fatte= []   #una lista che evita la ripetizione della stessa domanda sulla stessa partita
+valido= True   #una che convalida l'inizio partita
+
 for prova in range(1, int((len(righe)/2) + 1)):
-    n.append(prova)
-while valido:
+    n.append(prova)  #riempio la lista n con tutte le possibili partite
+    
+while valido:   #finchè non verrà digitato un numero corretto non parte il gioco
     try:
         partite=int((input(" Quanti round vuoi fare?  Digita un numero compreso tra 1 e " + str(int(len(righe)/2)) + " ---> ")))
-        rounde= partite
+        rounde= partite   #variabile che determinerà la percentuale a fine gioco
         if(partite not in n):
             valido= True
             print("Riprova, non è un numero compreso tra 1 e " + str(int(len(righe)/2)))
         else:
             valido= False
     except ValueError:
-            print("Hai digitato anche lettere, riprova")
+            print("Hai digitato anche lettere o simboli, riprova")
+            
 while (partite > 0):
     pippo= random.randrange(0, len(righe), 2)
     if (pippo not in fatte):
@@ -35,9 +41,12 @@ while (partite > 0):
             else:
                 punteggio= 0
         partite-=1
-        fatte.append(pippo)
+        fatte.append(pippo)   #riempio la lista fatte con la domanda già fatta così da non ripeterla
+        
 print("Hai totalizzato " + str(punteggio) + " come punteggio")
+
 percentuale= punteggio / rounde
+
 if percentuale < 0.25:
     print("mmm... bisogna allenarci di più! Ci vediamo alla prossima partita ;-)")
 elif percentuale >= 0.25 and percentuale < 0.50:
